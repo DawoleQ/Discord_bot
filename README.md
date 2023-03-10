ddc2f335eaa36c5daa3895966d2ea3f2000ea12f
Dawid Gardzielewski <thelastcinnamonbun@gmail.com>
2023-03-10 23:01:19 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,12 +1,35 @@
-A discord bot made by me (still in development)
-Made in discord.py 2.0 and requires python 3.8
+Hello!!!!!!!!!
+This is a tutorial repo for discord.py rewrite.
+(This tutorial repo is not finished yet. You will have to wait for that :D)
 
-For it to work on your PC you have to create a .env file in the bot's root directory (where main.py is)
-in there you have to add two variables: TOKEN and OWNER_ID like so:
-TOKEN = 'your_bot_token_here'
-OWNER_ID = bot_owner_id_here
+What you need:
+    -python 3.8 (I use python 3.8.10 specifically)
+    -discord.py with voice (run "pip install discord.py[voice]")
+    -dotenv (run "pip install python-dotenv")
 
-Also, you should add your bot to a server with the "bot" and "applications.commands" scopes
-you should also enable all intents for your bot (All of this is done on the discord developer portal [But you probably already know that hehe])
+What is included:
+    -A well commented and readable code showing you how to do basic and more complicated things in discord.py
+    -Cog implementation
+    -Simple json data managment
 
-For now it does literally nothing XD you can use this as an template
+How to start:
+    -First, create your application and the bot. You can do that here: https://discord.com/developers/applications
+
+    -Second, create a .env file in the bot's root directory (where main.py is) the file has "no name", it's just .env
+
+    -Third, in the .env file you created, make 2 variables: TOKEN and OWNER_ID like so:
+        TOKEN = 'token_here'
+        OWNER_ID = owner_id_here
+
+    -Then, read the files and the comments carefully and try to understand how this all works.
+
+    -When you're done with that, try to run the bot by typing "python main.py" in the terminal, see how it works for yourself.
+
+    -And last but not least, have fun with it! Once you're done checking this repo out, add more commands, functionality and style to the bot. Whatever comes to mind!
+        If you have a idea, but don't know how to put it into code, check out the docs, search through the internet or you can even ask me directly! If I know the solution to your problem, I'll definitely help!
+
+        You can find the docs here: https://discordpy.readthedocs.io/en/stable/
+        Or ask me on discord: DawoleQ#3486
+
+Well, off you go now! Have fun with discord.py rewrite!!! ❤️
+
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-import discord
-from discord.ext import commands
-from discord import app_commands
-import dotenv
-import os
-dotenv.load_dotenv()
+import discord #main discord module
+from discord.ext import commands #command module
+from discord import app_commands #app commands module
+import dotenv #module for .env file
+import datetime #module for getting the time
+import os 
+dotenv.load_dotenv() #load the .env file
 
 # Initialize bot
-intents = discord.Intents.all()
-OWNER_ID = os.getenv('OWNER_ID')
-client = commands.Bot(command_prefix='$', owner_id=OWNER_ID, intents=intents)
+intents = discord.Intents.all() #intent are essential to run the bot since discord.py rewrite
+OWNER_ID = os.getenv('OWNER_ID') #setting the owner ID
+client = commands.Bot(command_prefix='$', owner_id=OWNER_ID, intents=intents) #creating the bot user
 
+#The on_ready() event is executed when bot initialization (the thing above this) is done
 @client.event
 async def on_ready():
     # Load extensions
@@ -29,7 +31,7 @@ async def on_guild_join(guild):
     guildV = client.get_guild(guildId)
     print(f'Bot joined a server named {guildV}')
 
-# Error handler
+# Error handler (will just display the error for naw)
 @client.event
 async def on_command_error(ctx, error):
     await ctx.send(error)
@@ -39,17 +41,27 @@ async def on_command_error(ctx, error):
 async def app_command_test(ctx):
     await ctx.response.send_message('test succesful')
 
-# tree syncing command
+# tree syncing command (this is used to sync changes or add new app commands as they are stored and processed by discord itself, not by the bot)
 @client.command(name='treesync', aliases=['sync'])
 @commands.is_owner()
 async def treesync(ctx):
     try: 
-        client.tree.add_command(app_command_test, override=True)
         await client.tree.sync()
     except Exception as error:
         await ctx.send(f'Syncing error: {error}')
     else: await ctx.send('Syncing succesful')
 
+#This commands will run every time a command is run
+@client.event
+async def on_command(ctx):
+    #ctx is the context of the current command
+    user = ctx.author
+    command = ctx.command
+    date = datetime.now()
+    # this gets the current date in a specified format
+    time = date.strftime(r"%I:%M %p")
+    print(f'{user} used {command} at: {time}')
+
 # running the client
-TOKEN = os.getenv('TOKEN')
-client.run(TOKEN)
+TOKEN = os.getenv('TOKEN') #this function gets the TOKEN from the .env file we opened at the very top of this code (line 6)
+client.run(TOKEN) #this initializes the bot and must ALWAYS be at the very end of our main bot file
